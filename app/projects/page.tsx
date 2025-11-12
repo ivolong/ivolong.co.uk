5428f389950acfb1d034dc8ba755eb34bde679c6
ivo <ivolong@gmail.com>
2025-11-11 20:58:15 -0500

    feat: improve descriptions on the projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { FaReact, FaNode } from "react-icons/fa";
+import { FaGolang } from "react-icons/fa6";
 import { RiNextjsLine, RiTailwindCssFill } from "react-icons/ri";
 import { SiTypescript } from "react-icons/si";
 
@@ -10,9 +11,11 @@ import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 const projects = [
   {
     name: "ivolong.co.uk",
-    href: "https://github.com/ivolong/ivolong",
+    href: "https://github.com/ivolong/ivolong.co.uk",
     description:
       "My personal website. Designed and built to showcase my life and work, whilst also being a great opportunity to try out some new technologies.",
+    justification:
+      "My existing website was old school - time to learn some new skills.",
     skills: [
       {
         title: "React",
@@ -33,6 +36,7 @@ const projects = [
     href: "https://github.com/ivolong/hltv-news-bot",
     description:
       "RSS reader and Discord application that delivers notifications when your favourite news website publishes an article.",
+    justification: "To earn the 'Verified Bot Developer' badge on Discord.",
     skills: [
       {
         title: "Node.js",
@@ -41,6 +45,20 @@ const projects = [
       {
         title: "TypeScript",
         icon: <SiTypescript />,
+      },
+    ],
+  },
+  {
+    name: "Ubuntu Server Status Reporter",
+    href: "https://github.com/ivolong/home-server-status-reporter",
+    description:
+      "Web app for displaying system resource usage and service health on my Ubuntu home server.",
+    justification:
+      "Existing solutions were more memory-intensive than I liked and offered features I did not need.",
+    skills: [
+      {
+        title: "Go",
+        icon: <FaGolang />,
       },
     ],
   },
@@ -63,6 +81,11 @@ export default function Projects() {
               </CardHeader>
               <CardContent>
                 <p>{project.description}</p>
+              </CardContent>
+              <CardContent>
+                <p>
+                  <b>Why did I make it:</b> {project.justification}
+                </p>
               </CardContent>
               <CardContent>
                 {project.skills && (
